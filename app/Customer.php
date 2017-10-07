be8fd5fe3661e888f4bf26414c637a1d6607c616
Arkhe Salcedo <arkhesalcedo@Arkhes-MacBook-Pro.local>
2017-10-07 16:58:44 -0400

    updates

--- app/Customer.php
+++ app/Customer.php
@@ -20,21 +20,22 @@ class Customer extends Model
 
     	$success = false;
 
-    	if ($job->last_run) {
-    		$start = \Carbon\Carbon::parse($job->last_run, 'UTC');
-    	} else {
+    	if ($dateStart) {
     		$start = \Carbon\Carbon::parse($dateStart, 'UTC');
+    	} else {
+    		if (!$job->last_run) {
+    			$start = \Carbon\Carbon::today('UTC')->subDays(2);
+    		} else {
+    			$start = \Carbon\Carbon::parse($job->last_run, 'UTC');
+    		}
     	}
         
         $end = \Carbon\Carbon::parse($start->toDateTimeString(), 'UTC')->addDay();
 
-        echo $start;
-        echo $end;
-
         $startHour = $today->diffInHours($start);
         $endHour = $today->diffInHours($end);
 
-        try {
+        if ($end->diffInDays($today) >= 2) {
         	$amazonOrders = new AmazonOrderList($store);
 	        $amazonOrders->setLimits('Created', "- $startHour hours", "- $endHour hours");
 	        $amazonOrders->setOrderStatusFilter(['Shipped']);
@@ -43,36 +44,38 @@ class Customer extends Model
 	        $orders = $amazonOrders->getList();
 
 	        foreach ($orders as $order) {
-	            $customer = \App\Customer::firstOrNew([
-	                'name' => $order->getBuyerName(),
-	                'shipping_purchase_date' => \Carbon\Carbon::parse($order->getPurchaseDate())->toDateTimeString()
-	            ]);
-	            
-	            $customer->name = $order->getBuyerName();
-	            $customer->store = $store;
-	            $customer->shipping_purchase_date = \Carbon\Carbon::parse($order->getPurchaseDate())->toDateTimeString();
-	            $customer->shipping_name = $order->getShippingAddress()['Name'];
-	            $customer->shipping_address_1 = $order->getShippingAddress()['AddressLine1'];
-	            $customer->shipping_address_2 = $order->getShippingAddress()['AddressLine2'];
-	            $customer->shipping_address_3 = $order->getShippingAddress()['AddressLine3'];
-	            $customer->shipping_city = $order->getShippingAddress()['City'];
-	            $customer->shipping_county = $order->getShippingAddress()['County'];
-	            $customer->shipping_district = $order->getShippingAddress()['District'];
-	            $customer->shipping_state_or_region = $order->getShippingAddress()['StateOrRegion'];
-	            $customer->shipping_postal_code = $order->getShippingAddress()['PostalCode'];
-	            $customer->shipping_country_code = $order->getShippingAddress()['CountryCode'];
-	            $customer->shipping_phone = $order->getShippingAddress()['Phone'];
-	            $customer->shipping_amount = $order->getOrderTotal()['Amount'];
-	            $customer->shipping_order_count = $order->getNumberOfItemsShipped();
+	        	try {
+	        		$customer = \App\Customer::firstOrNew([
+		                'name' => $order->getBuyerName(),
+		                'shipping_purchase_date' => \Carbon\Carbon::parse($order->getPurchaseDate())->toDateTimeString()
+		            ]);
+		            
+		            $customer->name = $order->getBuyerName();
+		            $customer->store = $store;
+		            $customer->shipping_purchase_date = \Carbon\Carbon::parse($order->getPurchaseDate())->toDateTimeString();
+		            $customer->shipping_name = $order->getShippingAddress()['Name'];
+		            $customer->shipping_address_1 = $order->getShippingAddress()['AddressLine1'];
+		            $customer->shipping_address_2 = $order->getShippingAddress()['AddressLine2'];
+		            $customer->shipping_address_3 = $order->getShippingAddress()['AddressLine3'];
+		            $customer->shipping_city = $order->getShippingAddress()['City'];
+		            $customer->shipping_county = $order->getShippingAddress()['County'];
+		            $customer->shipping_district = $order->getShippingAddress()['District'];
+		            $customer->shipping_state_or_region = $order->getShippingAddress()['StateOrRegion'];
+		            $customer->shipping_postal_code = $order->getShippingAddress()['PostalCode'];
+		            $customer->shipping_country_code = $order->getShippingAddress()['CountryCode'];
+		            $customer->shipping_phone = $order->getShippingAddress()['Phone'];
+		            $customer->shipping_amount = $order->getOrderTotal()['Amount'];
+		            $customer->shipping_order_count = $order->getNumberOfItemsShipped();
 
-	            $customer->save();
+		            $customer->save();
 
-	            $count++;
+		            $count++;
+		        } catch (Exception $e) {
+		        	Log::info('Showing user profile for user: '. $e);
+		        }
 	        }
 
 	        $success = true;
-        } catch (Exception $e) {
-        	Log::info('Showing user profile for user: '. $e);
         }
 
         $job->store = $store;
