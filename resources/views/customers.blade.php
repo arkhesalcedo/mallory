8f6b8e2074aa00689b09246af4da18ebefc4b811
Arkhe Salcedo <arkhesalcedo@Arkhes-MacBook-Pro.local>
2017-10-09 10:58:17 -0400

    updates

--- resources/views/customers.blade.php
+++ resources/views/customers.blade.php
@@ -9,11 +9,11 @@
                 <div class="panel-heading">Stats</div>
 
                 <div class="panel-body">
-                    <mm-graph url="/stats/customersByMonth" store="US" type="line" label="Average US Customers Per Month"></mm-graph>
+                    <mm-graph url="/stats/customersByMonth" store="US" type="line" label="US Customers Per Month"></mm-graph>
 
-                    <mm-graph url="/stats/customersByMonth" store="CA" type="line" label="Average CA Customers Per Month" color="75, 192, 192"></mm-graph>
+                    <mm-graph url="/stats/customersByMonth" store="CA" type="line" label="CA Customers Per Month" color="75, 192, 192"></mm-graph>
 
-                    <mm-graph url="/stats/customersByMonth" store="UK" type="line" label="Average UK Customers Per Month" color="153, 102, 255"></mm-graph>
+                    <mm-graph url="/stats/customersByMonth" store="UK" type="line" label="UK Customers Per Month" color="153, 102, 255"></mm-graph>
                 </div>
             </div>
         </div>
