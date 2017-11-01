caa83f8aa49a460e4ccf9a23440c018ac1e4a301
Arkhe Salcedo <arkhesalcedo@Arkhes-MacBook-Pro.local>
2017-10-31 21:39:33 -0400

    updates

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -64,11 +64,7 @@ class HomeController extends Controller
             $excel->setTitle('Customer List');
 
             $excel->sheet('Sheet 1', function($sheet) use($customers) {
-                \App\Customer::chunk(500, function($customers) use($sheet){
-                    foreach ($customers as $customer) {
-                        $sheet->fromArray($customer);
-                    }
-                });
+                $sheet->fromArray($customers);
             });
         })->export('csv');
     }
